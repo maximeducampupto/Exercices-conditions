c26bc81f2d5a88f31467fc86411139e3570c8631
Max <maximeducampupto@gmail.com>
2018-10-18 16:54:34 +0200

    Initial commit

---
+++ exercice1.js
@@ -0,0 +1,18 @@
+/**
+ * Created by sstienface on 16/10/2018.
+ */
+
+
+
+/* Modifier le code ci dessous pour réaliser l'exercice */
+var x = 5,
+    y = 10;
+
+
+if (x > y) {
+    document.getElementById('monDiv').innerHTML = "La variable x est supérieure à la variable y";
+} else {
+    document.getElementById('monDiv').innerHTML = "La variable x est inférieure à la variable y";
+}
+
+
---
+++ exercice3/exercice3.js
@@ -0,0 +1,21 @@
+/**
+ * Created by sstienface on 16/10/2018.
+ */
+
+
+
+/* Modifier le code ci dessous pour réaliser l'exercice */
+
+var x = 5,
+    y = 5;
+
+x += 1;
+
+if(x == y)
+{
+    console.log('X est strictement égal à Y');
+}
+else
+{
+    console.log('X n\'est pas égal à Y');
+}
